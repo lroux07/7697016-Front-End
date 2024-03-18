43eb8b932aed4163976c7eca5d15fd733c4de37c
laelian-r <laelian.roux@gmail.com>
2024-03-18 15:23:55 +0100

    first commit

---
+++ pieces.js
@@ -0,0 +1,11 @@
+// FICHES
+alert("test js")
+
+// const section = document.querySelector('.fiches');
+
+// const image = document.createElement('img');
+// const title = document.createElement('h2');
+// const price = document.createElement('p');
+// const optique = document.createElement('p');
+
+// section.style.backgroundColor = "blue"
